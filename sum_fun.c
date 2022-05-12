459e7d766cff24cd666d7d36df93550d3509d138
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-12 20:42:39 +0530

    A basic function to add two numbers

---
+++ sum_fun.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int sum(int a,int b){
+        return a+b;
+    }
+
+    int num1,num2;
+    printf("Enter number 1:");
+    scanf("%d",&num1);
+    printf("Enter number 2:");
+    scanf("%d",&num2);
+    printf("sum of %d and %d is %d",num1,num2,sum(num1,num2));
+    return 0;
+}
