50b379b23403e1466de690a4a1188f9b91ea57ce
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-12 20:40:10 +0530

    selection sort c program

---
+++ selection_sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5];
+    int n=5,temp;
+    for(int i=0;i<n;i++){
+        printf("Enter the element %d :",i);
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+}
