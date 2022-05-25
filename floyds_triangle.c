f61f64b66877e09e576d62570b07a6b29b418fd7
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-25 07:30:10 +0530

    Add files via upload

---
+++ floyds_triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int n,val=1;
+	printf("Enter the number of rows : ");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			printf("%d ",val);
+			val++;
+		}
+		printf("\n");
+	}
+}
